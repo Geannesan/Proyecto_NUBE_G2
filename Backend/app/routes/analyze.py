d58543236afe2ab2c2bb8c8e09657af5adacbb0f
Geanella <geajsant@espol.edu.ec>
2026-06-23 20:03:02 -0500

    feat(backend): actualizar ruta de análisis

--- Backend/app/routes/analyze.py
+++ Backend/app/routes/analyze.py
@@ -1,38 +1,75 @@
 import os
+import uuid
+
 
 from fastapi import APIRouter
 from fastapi import UploadFile
 from fastapi import File
+from fastapi import HTTPException
+
 
 from app.detector.detector import detect_fake
 
+
+
 router = APIRouter()
+
+
 
 @router.post("/analyze")
 async def analyze(
     file: UploadFile = File(...)
 ):
 
+
+    # validar imagen
+
+    if not file.content_type.startswith("image"):
+
+        raise HTTPException(
+            status_code=400,
+            detail="El archivo debe ser una imagen"
+        )
+
+
+
+    # crear carpeta
+
     os.makedirs(
         "uploads",
         exist_ok=True
     )
 
+
+
+    # nombre único
+
     file_path = (
-        f"uploads/{file.filename}"
+        f"uploads/{uuid.uuid4()}_{file.filename}"
     )
+
+
+
+    # guardar imagen
 
     with open(
         file_path,
         "wb"
     ) as buffer:
 
+
         buffer.write(
             await file.read()
         )
 
+
+
+    # enviar al modelo
+
     result = detect_fake(
         file_path
     )
+
+
 
     return result
